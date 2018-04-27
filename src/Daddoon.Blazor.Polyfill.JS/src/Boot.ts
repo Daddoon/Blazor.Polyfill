8ce271225efb10ebd1ea6bf2a26f743bd0526246
Guillaume ZAHRA <guillaume.zahra@daddoon.com>
2018-04-27 15:08:31 +0200

    Added Polyfill and fix on Symbol for IE11
    Added Polyfill and fix on Symbol for IE11

---
+++ src/Daddoon.Blazor.Polyfill.JS/src/Boot.ts
@@ -0,0 +1,40 @@
+﻿/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
+import 'core-js/es6/promise'
+import 'whatwg-fetch';
+
+declare var Symbol;
+
+function IsIE() {
+
+    if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+        return true;
+    }
+
+    return false;
+}
+
+function blazorPolyfill() {
+
+    // IE11 has somme issue about setter with deep recursion
+    // with Symbol polyfill. We need to use the useSimple
+    // option from core-js
+    if (IsIE()) {
+        Symbol.useSimple();
+    }
+}
+
+blazorPolyfill();
