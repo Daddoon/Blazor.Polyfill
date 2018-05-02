43288012a03b04f7ff665e926dcf32a7b7798e9f
Daddoon <guillaume.zahra@daddoon.com>
2018-05-02 20:13:06 +0200

    - Updated Blazor.Polyfill for buggy WebAssembly on iOS. Still need to do some test (no iPhone present am)

--- src/Daddoon.Blazor.Polyfill.JS/src/Boot.ts
+++ src/Daddoon.Blazor.Polyfill.JS/src/Boot.ts
@@ -18,35 +18,70 @@ import 'whatwg-fetch';
 
 declare var Symbol;
 declare var document;
+declare var self;
 
-function IsIE() {
+(function () {
+    function IsIE() {
 
-    if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-        return true;
+        if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+            return true;
+        }
+
+        return false;
     }
 
-    return false;
-}
+    function blazorPolyfill() {
 
-function blazorPolyfill() {
+        // IE11 has somme issue about setter with deep recursion
+        // with Symbol polyfill. We need to use the useSimple
+        // option from core-js
+        if (IsIE()) {
+            Symbol.useSimple();
 
-    // IE11 has somme issue about setter with deep recursion
-    // with Symbol polyfill. We need to use the useSimple
-    // option from core-js
-    if (IsIE()) {
-        Symbol.useSimple();
+            //Adding document.baseURI for IE
+            //Additional checks for Internet Explorer"
+            if (document.baseURI == null || document.baseURI == undefined) {
+                //IE case
+                var port = "";
+                if (location.port != undefined && location.port != null && location.port != "")
+                    port = ":" + location.port;
 
-        //Adding document.baseURI for IE
-        //Additional checks for Internet Explorer"
-        if (document.baseURI == null || document.baseURI == undefined) {
-            //IE case
-            var port = "";
-            if (location.port != undefined && location.port != null && location.port != "")
-                port = ":" + location.port;
-
-            document.baseURI = location.protocol + "//" + location.hostname + port + "/";
+                document.baseURI = location.protocol + "//" + location.hostname + port + "/";
+            }
         }
     }
-}
 
-blazorPolyfill();
+    function checkWebAssemblySupport() {
+        // from https://github.com/brion/min-wasm-fail/blob/master/min-wasm-fail.js
+
+        // detect WebAssembly support and load either WASM or ASM version of Blazor
+        // actually, Blazor will fallback to asmjs if the WebAssembly namespace is not
+        //found. Removing WebAssembly namespace if buggy iOS browser detected
+        var support = (typeof WebAssembly === 'object');
+        if (!support) {
+            //WebAssembly not supported by default. nothing to do
+            return;
+        }
+
+        try {
+            const bin = new Uint8Array([0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 1, 127, 1, 127, 3, 2, 1, 0, 5, 3, 1, 0, 1, 7, 8, 1, 4, 116, 101, 115, 116, 0, 0, 10, 16, 1, 14, 0, 32, 0, 65, 1, 54, 2, 0, 32, 0, 40, 2, 0, 11]);
+            const mod = new WebAssembly.Module(bin);
+            const inst = new WebAssembly.Instance(mod, {});
+            // test storing to and loading from a non-zero location via a parameter.
+            // Safari on iOS 11.2.5 returns 0 unexpectedly at non-zero locations
+            support = (inst.exports.test(4) !== 0);
+        }
+        catch {
+            // Some version of iOS crash instead of returning a wrong value. Preventing the crash with this try/catch.
+            support = false;
+        }
+
+        if (support == false) {
+            //If support value changed, something wrong happened. We must delete WebAssembly namespace then.
+            delete self.WebAssembly;
+        }
+    }
+
+    blazorPolyfill();
+    checkWebAssemblySupport();
+})();
